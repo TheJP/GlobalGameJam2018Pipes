bf673022f1ab4cc1bf40016d98301388427f32ad
EdithBirrer <edith.birrer@hslu.ch>
2018-01-27 20:09:46 +0100

    Number display on table asset tile

--- GlobalGameJam2018Pipes/Assets/Scripts/Asset.cs
+++ GlobalGameJam2018Pipes/Assets/Scripts/Asset.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Asset : MonoBehaviour
 {
@@ -8,15 +10,44 @@ public class Asset : MonoBehaviour
     public int Column;
     public int tileSizeX;
     public int tileSizeZ;
-
+    
     private GameObject assetObj;
-    private int count;
+    
+    /*[HideInInspector]*/
+    public int pipeCount;  // number of assets stored here
+    public PipeType? pipeType;
+
+    private Text textObj;
 
 
     // Use this for initialization
     void Start ()
     {
         gameObject.transform.localScale = new Vector3 (tileSizeX, 1, tileSizeZ);
+
+        textObj = GetComponentInChildren<Text>();
+
+        // TODO only display text if there is something on this field
+        if (pipeType != null)
+            UpdateText();
+        
+    }
+
+    public void SetPipeType(PipeType type)
+    {
+        pipeType = type;
+    }
+
+    public void SetCount (int count)
+    {
+        this.pipeCount = count;
+        UpdateText();
+    }
+
+
+    private void UpdateText()
+    {
+        textObj.text =  pipeCount + "x";
     }
 
     public void Test ()

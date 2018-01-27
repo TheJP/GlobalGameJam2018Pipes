ef28105ba779de6090aae69fde8a53971d52c29e
Florian Graf <floriangraf@gmx.ch>
2018-01-27 11:48:52 +0100

    Splitted tile into tile and display

--- GlobalGameJam2018Pipes/Assets/Scripts/Tile.cs
+++ GlobalGameJam2018Pipes/Assets/Scripts/Tile.cs
@@ -6,14 +6,10 @@ public class Tile : MonoBehaviour
 {
     public int Row;
     public int Column;
-    public int tileSize;
-
-    private List<GameObject> pipes = new List<GameObject>(2);
 
     // Use this for initialization
     void Start()
     {
-        gameObject.transform.localScale = new Vector3(tileSize, 1, tileSize);
     }
 
     public void Test()

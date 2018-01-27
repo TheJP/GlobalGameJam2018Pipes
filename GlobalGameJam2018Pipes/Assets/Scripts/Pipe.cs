9c0e670b9e71c36b76e8e1bb9343a174aba6ae0c
Florian Graf <floriangraf@gmx.ch>
2018-01-27 16:22:53 +0100

    Added pipe script with flow direction configurations on all pipe prefabs

---
+++ GlobalGameJam2018Pipes/Assets/Scripts/Pipe.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class Pipe : MonoBehaviour
+{
+    [SerializeField] private FlowDirection fromLeft;
+
+    [SerializeField] private FlowDirection fromTop;
+
+    [SerializeField] private FlowDirection fromRight;
+
+    [SerializeField] private FlowDirection fromBottom;
+
+    [SerializeField] private int rotation;
+
+    public int Rotation
+    {
+        get { return rotation; }
+        set { rotation = value % 4; }
+    }
+
+    public FlowDirection FromLeft => GetRotatedFlowDirection(fromLeft, fromTop, fromRight, fromBottom);
+
+    public FlowDirection FromTop => GetRotatedFlowDirection(fromTop, fromRight, fromBottom, fromLeft);
+
+    public FlowDirection FromRight => GetRotatedFlowDirection(fromRight, fromBottom, fromLeft, fromTop);
+
+    public FlowDirection FromBottom => GetRotatedFlowDirection(fromBottom, fromLeft, fromTop, fromRight);
+    
+    private FlowDirection GetRotatedFlowDirection(
+        FlowDirection one,
+        FlowDirection two,
+        FlowDirection three,
+        FlowDirection four)
+    {
+        switch (rotation)
+        {
+        default:
+        case 0:
+            return one;
+        case 1:
+            return two;
+        case 2:
+            return three;
+        case 3:
+            return four;
+        }
+    }
+}

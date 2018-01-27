951234ca246a76a68fda3022c4ab2e12db4c5155
Florian Graf <floriangraf@gmx.ch>
2018-01-27 11:49:23 +0100

    Tile can now build and remove pipes

--- GlobalGameJam2018Pipes/Assets/Scripts/Tile.cs
+++ GlobalGameJam2018Pipes/Assets/Scripts/Tile.cs
@@ -1,15 +1,54 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 
 public class Tile : MonoBehaviour
 {
     public int Row;
     public int Column;
 
-    // Use this for initialization
-    void Start()
+    public GameObject pipeStraight;
+    public GameObject pipeTurn;
+    public GameObject pipeLeftRight;
+    public GameObject pipeOverUnder;
+    public GameObject pipeXIntersection;
+    public GameObject pipeTIntersection;
+
+    public bool hasPipe;
+    
+    public bool BuildPipe(PipeType pipeType)
     {
+        if(hasPipe)
+        {
+            return false;
+        }
+
+        switch(pipeType)
+        {
+        case PipeType.Straight:
+            Instantiate(pipeStraight, transform);
+            return hasPipe = true;
+        case PipeType.Turn:
+            Instantiate(pipeTurn, transform);
+            return hasPipe = true;
+        case PipeType.LeftRight:
+            Instantiate(pipeLeftRight, transform);
+            return hasPipe = true;
+        case PipeType.UnderOver:
+            Instantiate(pipeOverUnder, transform);
+            return hasPipe = true;
+        case PipeType.TIntersection:
+            Instantiate(pipeTIntersection, transform);
+            return hasPipe = true;
+        case PipeType.XIntersection:
+            Instantiate(pipeXIntersection, transform);
+            return hasPipe = true;
+        default:
+            return false;
+        }
+    }
+
+    public void RemovePipe()
+    {
+        hasPipe = false;
     }
 
     public void Test()

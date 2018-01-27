42d2fe5d044a5ed3e4f3480605a71dc25e705906
Florian Graf <floriangraf@gmx.ch>
2018-01-27 08:40:46 +0100

    Added basic inventory

---
+++ GlobalGameJam2018Pipes/Assets/Scripts/Inventory.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+  [SerializeField] private int pipeStraigthCount;
+
+  [SerializeField] private int pipeTurnCount;
+
+  [SerializeField] private int pipeLeftRightCount;
+
+  [SerializeField] private int pipeOverUnderCount;
+
+  [SerializeField] private int pipeTIntersectionCount;
+
+  [SerializeField] private int pipeXIntersectionCount;
+
+  public int PipeStraightCount
+  {
+    get { return pipeStraigthCount; }
+    set { pipeStraigthCount = value; }
+  }
+
+  public int PipeTurnCount
+  {
+    get { return pipeTurnCount; }
+    set { pipeTurnCount = value; }
+  }
+
+  public int PipeLeftRightCount
+  {
+    get { return pipeLeftRightCount; }
+    set { pipeLeftRightCount = value; }
+  }
+
+  public int PipeOverUnderCount
+  {
+    get { return pipeOverUnderCount; }
+    set { pipeOverUnderCount = value; }
+  }
+
+  public int PipeTIntersectionCount
+  {
+    get { return pipeTIntersectionCount; }
+    set { pipeTIntersectionCount = value; }
+  }
+
+  public int PipeXIntersectionCount
+  {
+    get { return pipeXIntersectionCount; }
+    set { pipeXIntersectionCount = value; }
+  }
+}

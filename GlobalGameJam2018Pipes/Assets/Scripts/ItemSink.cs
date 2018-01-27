9fb78531822fe5bad5d1a9431319fcf2b74239c2
Stefan Winterberger <stefanwinterb88@hotmail.com>
2018-01-27 12:42:59 +0100

    created ItemSink

---
+++ GlobalGameJam2018Pipes/Assets/Scripts/ItemSink.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSink : MonoBehaviour
+{
+
+    public bool singlePlayer = true;
+
+    public void ProcessSinkItem(GameObject item)
+    {
+        if (singlePlayer)
+        {
+            Destroy(item);
+        }
+        else
+        {
+            //Do Network Stuff
+        }
+
+    }
+}
